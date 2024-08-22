6f600a3f65f03e63ba21d9c441867e23776484c1
junesung1004 <junecoding1004@gmail.com>
2024-08-22 13:56:28 +0900

    애니메이션 구현중

--- js/main.js
+++ js/main.js
@@ -3,6 +3,10 @@ const descLeft = document.querySelector(".desc-img-left");
 const descMiddle = document.querySelector(".desc-img-middle");
 const descRight = document.querySelector(".desc-text-right");
 
+const title = document.querySelector(".intro-title");
+const desc = document.querySelectorAll(".intro-desc");
+const slide = document.querySelector(".slide");
+
 let observer = new IntersectionObserver((e) => {
   e.forEach((item) => {
     if (item.isIntersecting) {
@@ -19,6 +23,11 @@ images.forEach((img) => {
   observer.observe(img);
 });
 
+desc.forEach((img) => {
+  observer.observe(img);
+});
+
 observer.observe(descLeft);
 observer.observe(descMiddle);
 observer.observe(descRight);
+observer.observe(title);
